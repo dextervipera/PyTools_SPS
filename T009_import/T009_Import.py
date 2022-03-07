5888bf507dba78aa82547021b929fb19f1be6cee
KKK <dextervipera@gmail.com>
2022-03-07 07:05:29 +0100

    T009 data load using pandas add

---
+++ T009_import/T009_Import.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+#Dataclasses
+
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import numpy as np
+import pandas as pd
+df=pd.read_csv("kolumny_Cu_slupek-objetosc.csv", sep=',',header=0)
